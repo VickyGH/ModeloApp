48b072105e3d3b91aa3d7cbc19a10e3b0daabfaf
VickyGh <creaturmac@CreaturMacs-Mac-mini.local>
2016-05-05 18:39:22 -0500

    Anexo de ventana txt_img

---
+++ app/src/main/java/com/example/creaturmac/modeloapp/txt_img.java
@@ -0,0 +1,13 @@
+package com.example.creaturmac.modeloapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class txt_img extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_txt_img);
+    }
+}
