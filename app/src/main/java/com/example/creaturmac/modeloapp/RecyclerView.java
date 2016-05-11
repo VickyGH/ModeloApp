417cf3ce4fc37487c0460a826f4f407692b33a00
VickyGh <creaturmac@CreaturMacsMini.lan>
2016-05-11 18:11:44 -0500

    No funciona y no le entendí Recicle View

--- app/src/main/java/com/example/creaturmac/modeloapp/RecyclerView.java
+++ app/src/main/java/com/example/creaturmac/modeloapp/RecyclerView.java
@@ -3,21 +3,27 @@ package com.example.creaturmac.modeloapp;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class RecyclerView extends AppCompatActivity {
+
+    private RecyclerView recView;
+
+    private ArrayList<Titular> datos;
 
     private ListView lstOpciones1;
     private TextView lblEtiqueta;
 
-    private Titular[] datos1 =
+    // ArrayList de tipo Titular
+   /* private Titular[] datos =
             new Titular[]{
                     new Titular("Título 1", "Subtítulo largo 1"),
                     new Titular("Título 2", "Subtítulo largo 2"),
@@ -34,12 +40,28 @@ public class RecyclerView extends AppCompatActivity {
                     new Titular("Título 13", "Subtítulo largo 13"),
                     new Titular("Título 14", "Subtítulo largo 14"),
                     new Titular("Título 15", "Subtítulo largo 15")
-            };
+            };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        //inicialización de la lista de datos de ejemplo
+        datos = new ArrayList<Titular>();
+        for(int i=0; i<50; i++)
+            datos.add(new Titular("Título " + i, "Subtítulo item " + i));
+
+        //Inicialización RecyclerView
+        recView = (RecyclerView)findViewById(R.id.RecView);
+        recView.setHasFixedSize(true);
+
+        final AdaptadorTitulares adaptador = new AdaptadorTitulares(this, datos);
+
+        recView.setAdapter(adaptador);
+
+        recView.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false));
 
         //-- .. -- .. -- .. -- .. --
         lblEtiqueta = (TextView)findViewById(R.id.lblEtiqueta);
@@ -47,7 +69,7 @@ public class RecyclerView extends AppCompatActivity {
 
         //-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
         AdaptadorTitulares adaptador1 =
-                new AdaptadorTitulares(this, datos1);
+                new AdaptadorTitulares(this, datos);
 
         lstOpciones1 = (ListView)findViewById(R.id.LstOpciones1);
         lstOpciones1.setAdapter(adaptador1);
@@ -94,7 +116,30 @@ public class RecyclerView extends AppCompatActivity {
     }
     //--
 
-    class AdaptadorTitulares extends RecyclerView.Adapter<AdaptadorTitulares.TitularesViewHolder>{
+    class AdaptadorTitulares extends ArrayAdapter<Titular> {
+
+        public AdaptadorTitulares(Context context, Titular[] datos1) {
+            super(context, R.layout.listitem_titular, datos);
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View item = inflater.inflate(R.layout.listitem_titular, null);
+
+            TextView lblTitulo = (TextView)item.findViewById(R.id.LblTitulo);
+            lblTitulo.setText(datos[position].getTitulo());
+
+            TextView lblSubtitulo = (TextView)item.findViewById(R.id.LblSubTitulo);
+            lblSubtitulo.setText(datos[position].getSubtitulo());
+
+            return(item);
+        }
+    }
+
+    public class AdaptadorTitulares
+            extends RecyclerView.Adapter<AdaptadorTitulares.TitularesViewHolder> {
+
+        private ArrayList<Titular> datos1;
 
         public static class TitularesViewHolder extends RecyclerView.ViewHolder {
 
@@ -113,18 +158,33 @@ public class RecyclerView extends AppCompatActivity {
                 txtSubtitulo.setText(t.getSubtitulo());
             }
         }
+        public AdaptadorTitulares(RecyclerView recyclerView, ArrayList<Titular> datos) {
+            this.datos = datos;
+        }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View item = inflater.inflate(R.layout.listitem_titular, null);
+        @Override
+        public TitularesViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
 
-            TextView lblTitulo = (TextView)item.findViewById(R.id.LblTitulo);
-            lblTitulo.setText(datos1[position].getTitulo());
+            View itemView = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.listitem_titular, viewGroup, false);
 
-            TextView lblSubtitulo = (TextView)item.findViewById(R.id.LblSubTitulo);
-            lblSubtitulo.setText(datos1[position].getSubtitulo());
+            TitularesViewHolder tvh = new TitularesViewHolder(itemView);
 
-            return(item);
+            return tvh;
+        }
+
+        @Override
+        public void onBindViewHolder(TitularesViewHolder viewHolder, int pos) {
+            Titular item = datos.get(pos);
+
+            viewHolder.bindTitular(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return datos.size();
         }
     }
+
+
 }
